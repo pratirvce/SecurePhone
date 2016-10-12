5374b24ec4c7dee76c8944ede098287d5bf3b269
Nolyda Tep <nolyda.tep@sjsu.edu>
2016-10-12 13:30:30 -0700

    Added a broadcast receiver. The main activity will start when phone starts/is restarted.

--- app/src/main/java/com/sjsu/securephone/theftdetector/BootCompleteReceiver.java
+++ app/src/main/java/com/sjsu/securephone/theftdetector/BootCompleteReceiver.java
@@ -3,16 +3,22 @@ package com.sjsu.securephone.theftdetector;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootCompleteReceiver";
+
     public BootCompleteReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Intent in = new Intent(context, MainActivity.class);
-        in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(in);
+            Log.d(TAG, "Boot completed. Main Activity will start now.");
+            Intent in = new Intent(context, MainActivity.class);
+            in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(in);
+
     }
 }
