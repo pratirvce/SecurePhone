7163b6ca00709e40cabacd80480b19b9b5547f88
pratirvce <pratirvce@gmail.com>
2016-10-11 20:13:55 -0700

    Initial commit

---
+++ app/src/main/java/com/sjsu/securephone/theftdetector/MainActivity.java
@@ -0,0 +1,152 @@
+package com.sjsu.securephone.theftdetector;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import pub.devrel.easypermissions.EasyPermissions;
+
+/**
+ * Created by Group 7 on 10/11/2016.
+ */
+
+public class MainActivity extends AppCompatActivity implements
+        EasyPermissions.PermissionCallbacks{
+
+    private boolean doubleBackToExitPressedOnce=false;
+
+    private static final int RC_READ_SMS = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tabanim_toolbar);
+        setSupportActionBar(toolbar);
+        readSMSPerm();
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new SIMDetectorFragment(), "SIM Info");
+        adapter.addFrag(new LocationFragment(), "Build Info");
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+            startActivity(intent);
+            finish();
+            System.exit(0);
+
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+            startActivity(intent);
+            finish();
+            System.exit(0);
+            return true;
+        }
+        if(id==R.id.action_share){
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = getString(R.string.share_text);
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void readSMSPerm() {
+        String[] perms = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_NETWORK_STATE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            sendAndOpenIntent();
+        } else {
+            // Ask for both permissions
+            Log.d("else", "part");
+            EasyPermissions.requestPermissions(this, "Allow Sim Info read?",
+                    RC_READ_SMS, perms);
+        }
+    }
+
+    public void sendAndOpenIntent() {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.tabanim_viewpager);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabanim_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+// EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        Log.d("", "onPermissionsGranted:" + requestCode + ":" + perms.size());
+        sendAndOpenIntent();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d("", "onPermissionsDenied:" + requestCode + ":" + perms.size());
+        Toast.makeText(getApplicationContext(), "Permission is Compulsory to Proceed", Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/sjsu/securephone/theftdetector/SIMDetectorFragment.java
@@ -0,0 +1,89 @@
+package com.sjsu.securephone.theftdetector;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Group 7 on 10/11/2016.
+ */
+
+public class SIMDetectorFragment extends Fragment {
+    @Bind(R.id.is_root)TextView root_text;
+    @Bind(R.id.is_root_available)TextView t1;
+    @Bind(R.id.super_user_text)TextView su_text;
+    @Bind(R.id.is_su_found)TextView t2;
+    @Bind(R.id.is_busy_box_installed)TextView t3;
+    @Bind(R.id.path)TextView t4;
+    @Bind(R.id.voicemail_tag)TextView voicemail;
+    @Bind(R.id.sim_serial)TextView sim_serial;
+    @Bind(R.id.device_id)TextView device_id;
+    @Bind(R.id.device_software_version)TextView device_software_version;
+    private static final String ARG_TEXT = "text";
+    private Tracker mTracker;
+
+    public SIMDetectorFragment() {
+    }
+
+    public static SIMDetectorFragment newInstance(String text) {
+        SIMDetectorFragment fragment = new SIMDetectorFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_TEXT, text);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.sim_detector, container, false);
+
+        /*Google Analytics: send screen Name*/
+
+        // Obtain the shared Tracker instance.
+        AnalyticsActivity application = (AnalyticsActivity) getActivity().getApplication();
+        mTracker = application.getTracker(AnalyticsActivity.TrackerName.APP_TRACKER);
+
+        // Send a screen view.
+        mTracker.setScreenName("SIM Info");
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        mTracker.send(new HitBuilders.ScreenViewBuilder()
+                .set("SIMInfo", "SIM Info")
+                .build());
+
+        /*
+        AdView mAdView = (AdView) view.findViewById(R.id.adView1);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        mAdView.loadAd(adRequest);*/
+
+        Log.d("on detail create view", "created");
+        ButterKnife.bind(this, view);
+        //GENERAL
+
+        TelephonyManager manager=(TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        t1.setText(manager.getSimCountryIso());
+        t2.setText(manager.getNetworkOperatorName());
+        t3.setText(manager.getSimOperatorName());
+        t4.setText(String.valueOf(manager.isNetworkRoaming()));
+        device_id.setText(manager.getDeviceId());
+        voicemail.setText(manager.getVoiceMailAlphaTag());
+        device_software_version.setText(manager.getDeviceSoftwareVersion());
+        sim_serial.setText(manager.getSimSerialNumber());
+        return view;
+    }
+
+}
+
