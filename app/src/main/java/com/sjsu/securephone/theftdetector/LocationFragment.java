4bf41045dea19a0a4c414f8b817cda7263da71aa
Nolyda Tep <nolyda.tep@sjsu.edu>
2016-10-13 15:08:46 -0700

    removed errors

--- app/src/main/java/com/sjsu/securephone/theftdetector/LocationFragment.java
+++ app/src/main/java/com/sjsu/securephone/theftdetector/LocationFragment.java
@@ -7,7 +7,6 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
@@ -20,38 +19,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.provider.Settings;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 import com.firebase.client.Firebase;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.android.gms.analytics.Tracker;
 
 //import com.google.android.gms.ads.AdRequest;
 //import com.google.android.gms.ads.AdView;
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * Created by Group 7 on 10/11/2016.
@@ -69,6 +41,8 @@ public class LocationFragment extends Fragment {
     private Tracker mTracker;
     private static final String ARG_TEXT = "text";
 
+    private static final String TAG = "LocationFragment";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -77,7 +51,8 @@ public class LocationFragment extends Fragment {
         View view = inflater.inflate(R.layout.location_info, container, false);
         /*Google Analytics: send screen Name*/
 
-        locationManager = (LocationManager) getView().getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
 
         longitudeValueBest = (TextView) getView().findViewById(R.id.longitudeValueBest);
         latitudeValueBest = (TextView) getView().findViewById(R.id.latitudeValueBest);
@@ -85,6 +60,8 @@ public class LocationFragment extends Fragment {
         latitudeValueGPS = (TextView) getView().findViewById(R.id.latitudeValueGPS);
         longitudeValueNetwork = (TextView) getView().findViewById(R.id.longitudeValueNetwork);
         latitudeValueNetwork = (TextView) getView().findViewById(R.id.latitudeValueNetwork);
+
+        return view;
     }
     private boolean checkLocation() {
         if (!isLocationEnabled())
@@ -121,13 +98,24 @@ public class LocationFragment extends Fragment {
             return;
         Button button = (Button) view;
         if(button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerGPS);
-            button.setText(R.string.resume);
+            try {
+                locationManager.removeUpdates(locationListenerGPS);
+                button.setText(R.string.resume);
+            }
+            catch(SecurityException e){
+                Log.e(TAG, "Location permission issue.");
+            }
+
         }
         else {
-            locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
-            button.setText(R.string.pause);
+            try {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
+                button.setText(R.string.pause);
+            }catch(SecurityException e){
+                Log.e(TAG, "Location permission issue.");
+            }
+
         }
     }
 
@@ -136,8 +124,12 @@ public class LocationFragment extends Fragment {
             return;
         Button button = (Button) view;
         if(button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerBest);
-            button.setText(R.string.resume);
+            try {
+                locationManager.removeUpdates(locationListenerBest);
+                button.setText(R.string.resume);
+            } catch (SecurityException e) {
+                Log.e(TAG, "Location permission issue.");
+            }
         }
         else {
             Criteria criteria = new Criteria();
@@ -148,9 +140,13 @@ public class LocationFragment extends Fragment {
             criteria.setPowerRequirement(Criteria.POWER_LOW);
             String provider = locationManager.getBestProvider(criteria, true);
             if(provider != null) {
-                locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
-                button.setText(R.string.pause);
-                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+                try{
+                    locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
+                    button.setText(R.string.pause);
+                    Toast.makeText(getActivity(), "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+                }catch(SecurityException e){
+                    Log.e(TAG, "Location permission issue.");
+                }
             }
         }
     }
@@ -161,14 +157,22 @@ public class LocationFragment extends Fragment {
             return;
         Button button = (Button) view;
         if(button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerNetwork);
-            button.setText(R.string.resume);
+            try{
+                locationManager.removeUpdates(locationListenerNetwork);
+                button.setText(R.string.resume);
+            }catch(SecurityException e){
+                Log.e(TAG, "Location permission issue.");
+            }
         }
         else {
-            locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
-            Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
-            button.setText(R.string.pause);
+            try{
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
+                Toast.makeText(getActivity(), "Network provider started running", Toast.LENGTH_LONG).show();
+                button.setText(R.string.pause);
+            }catch(SecurityException e){
+                Log.e(TAG, "Location permission issue.");
+            }
         }
     }
 
@@ -189,7 +193,7 @@ public class LocationFragment extends Fragment {
                     //Storing values to firebase
                     ref.child("currLoc/Longitude").setValue(longitudeBest);
                     ref.child("currLoc/Latitude").setValue(latitudeBest);
-                    Toast.makeText(MainActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Best Provider update", Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -224,7 +228,7 @@ public class LocationFragment extends Fragment {
                     //Storing values to firebase
                     ref.child("currLoc/Longitude").setValue(longitudeNetwork);
                     ref.child("currLoc/Latitude").setValue(latitudeNetwork);
-                    Toast.makeText(MainActivity.this, "Network Provider update", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Network Provider update", Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -266,7 +270,7 @@ public class LocationFragment extends Fragment {
                     //Storing values to firebase
                     ref.child("Longitude").setValue(longitudeGPS);
                     ref.child("Latitude").setValue(latitudeGPS);
-                    Toast.makeText(MainActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "GPS Provider update", Toast.LENGTH_SHORT).show();
                 }
             });
         }
